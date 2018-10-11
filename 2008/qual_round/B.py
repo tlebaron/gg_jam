a417f7206dac57c6566c289e8bd4d8a79877d57f
tlebaron <thomas.lebaron@free.fr>
2018-10-10 21:09:46 -0400

    2008/qual/B still pending

--- 2008/qual_round/B.py
+++ 2008/qual_round/B.py
@@ -8,18 +8,54 @@ nb_tests = int(i.readline())
 for test in range(0, nb_tests):
     startA = 0
     startB = 0
-    #trips[0] are from A to B, [1] form B to A.
+    #trips[0] are from A to B, [1] form B to A, [2] all
     #each is a list of [departure hour, dep min, arrival hour, arr min]
-    trips = [[],[]]
+    trips = [[],[],[]]
+    #instead of testing all minutes, just record interesting time
+    time = []
     turn_time = int(i.readline())
     [nb_trips_AB, nb_trips_BA] = [int(element) for element in i.readline().split(" ")]
-    
+
     for index, trips_number in enumerate([nb_trips_AB, nb_trips_BA]):
 	for trips_list_onway in range(0,trips_number):
 	    [departure, arrival] = i.readline().split(" ")
-	    trips[index].append([int(departure.split(":")[0]), int(departure.split(":")[1]), int(arrival.split(":")[0]), int(arrival.split(":")[1])])
+	    trip = [int(departure.split(":")[0])*60+int(departure.split(":")[1]), int(arrival.split(":")[0])*60+int(arrival.split(":")[1])]
+
+	    # just add turn time to record ready time instead of arrival time, useless
+	    trip[1] = trip[1] + turn_time
+
+	    trips[index].append(trip)
+	    trips[2].append(trip)
+
+	    if trip[0] not in time:
+		time.append(trip[0])
+	    if trip[1] not in time:
+		time.append(trip[1])
+
+    trips[2].sort()
+    time.sort()
+
+    d_A = False
+    d_B = False
+    a_A = False
+    a_B = False
+
+    for t in time:
+
+	#start with arrivals
+	for Atrips in trips[1]:
+	    if t == ### looks weird, sure there is a better method...
+
     
-    print test, turn_time, [nb_trips_AB, nb_trips_BA], trips
+
+    for trip in trips[2]:
+	if trip in trips[0]:
+	    departure = "A"
+	else:
+	    departure = "B"
+	
+
+    print test, turn_time, [nb_trips_AB, nb_trips_BA], trips[2], time
 
     o.write("Case #{0}: {1}\n".format(test+1, -1))
 
